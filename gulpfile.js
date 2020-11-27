fe1c7c6a4978256988dc7dcaa007e5ada244d4ce
Yifan Ai <ayfallen@hotmail.com>
2020-11-27 13:54:42 +1100

    Use Gulp to process files

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const {src, dest, series} = require('gulp');
+const del = require('del');
+const sass = require('gulp-dart-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+
+const SOURCE = 'src/';
+const DESTINATION = 'dist/css/';
+
+async function clean(cb) {
+    await del(`${DESTINATION}`);
+    cb();
+}
+
+function css(cb) {
+    src(`${SOURCE}/scss/*.scss`)
+		.pipe(sass())
+		.pipe(autoprefixer({
+			cascade: false
+		}))
+		.pipe(cleanCSS({compatibility: 'ie8'}))
+		.pipe(rename({ suffix: ".min" }))
+		.pipe(dest(`${DESTINATION}`));
+    cb();
+}
+
+exports.clean = clean;
+exports.css = css;
+
+exports.default = series(clean, css);
